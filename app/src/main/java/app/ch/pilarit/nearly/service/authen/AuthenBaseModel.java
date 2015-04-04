3affdfa0f78daadfaa75d75c7e95ea879bd938f6
geeksoccer <excelente74@gmail.com>
2015-04-04 22:15:15 +0700

    Ched Libs

---
+++ app/src/main/java/app/ch/pilarit/nearly/service/authen/AuthenBaseModel.java
@@ -0,0 +1,63 @@
+package app.ch.pilarit.nearly.service.authen;
+
+/**
+ * Created by ch_pilarit on 4/2/15 AD.
+ */
+public class AuthenBaseModel {
+
+    private String username;
+    private String password;
+    private String repassword;
+    private int roleid;
+
+    public AuthenBaseModel(){
+        this("","");
+    }
+
+    public AuthenBaseModel(String username, String password){
+        this(username, password, "");
+    }
+
+    public AuthenBaseModel(String username, String password, String repassword){
+        this(username,password,repassword,0);
+    }
+
+    public AuthenBaseModel(String username, String password, String repassword, int roleid){
+        this.username = username;
+        this.password = password;
+        this.repassword = repassword;
+        this.roleid = roleid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepassword() {
+        return repassword;
+    }
+
+    public void setRepassword(String repassword) {
+        this.repassword = repassword;
+    }
+
+    public int getRoleid() {
+        return roleid;
+    }
+
+    public void setRoleid(int roleid) {
+        this.roleid = roleid;
+    }
+}
---
+++ app/src/main/java/app/ch/pilarit/nearly/service/authen/AuthenLocal.java
@@ -0,0 +1,80 @@
+package app.ch.pilarit.nearly.service.authen;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.Map;
+
+import app.ch.pilarit.nearly.service.session.SessionLocal;
+
+/**
+ * Created by ch_pilarit on 4/2/15 AD.
+ */
+public class AuthenLocal {
+
+
+    private static SessionLocal sessionLocal;
+
+    public static boolean hasRegister(Context context) {
+        sessionLocal = SessionLocal.getInstance(context);
+        String usernameInit = String.valueOf(sessionLocal.get("username"));
+        String passwordInit = String.valueOf(sessionLocal.get("password"));
+
+        if(usernameInit.length() > 0 && passwordInit.length() > 0){
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public static SessionLocal register(Context context, Map map) {
+
+        if(map == null) return null;
+
+        sessionLocal = SessionLocal.getInstance(context);
+
+        for(Object key : map.keySet()){
+            sessionLocal.put(String.valueOf(key), map.get(key));
+        }
+
+        return  sessionLocal;
+    }
+
+    public static SessionLocal login(Context context, String username, String password) {
+
+        sessionLocal = SessionLocal.getInstance(context);
+
+        String usernameInit = String.valueOf(sessionLocal.get("username"));
+        String passwordInit = String.valueOf(sessionLocal.get("password"));
+
+        if(usernameInit.equals(username) && passwordInit.equals(password)){
+            sessionLocal.createSession();
+            return  sessionLocal;
+        }
+
+        return null;
+    }
+
+    public static SessionLocal login(Context context, String password) {
+
+        sessionLocal = SessionLocal.getInstance(context);
+
+        String passwordInit = String.valueOf(sessionLocal.get("password"));
+
+        if(passwordInit.equals(password)){
+            sessionLocal.createSession();
+            return  sessionLocal;
+        }
+
+        return null;
+    }
+
+    public static SessionLocal logout(Context context) {
+
+        sessionLocal = SessionLocal.getInstance(context);
+        sessionLocal.clear();
+
+        return  sessionLocal;
+    }
+}
